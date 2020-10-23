5c07e31e8f6686f90db604da613c441b00972a59
Magdiel Delgadillo <magdiel.delgadillo@gmail.com>
2020-10-23 11:09:12 -0700

    adds homepage mp4, dashboard, dashboard tabs

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def homepage():
     return render_template("main.html")
 
+@app.route('/dashboard/')
+def dashboard():
+    return render_template("dashboard.html")
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
