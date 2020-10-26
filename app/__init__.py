9fabdb450b28f80308832d06b253e1476e34ec1f
Magdiel Delgadillo <magdiel.delgadillo@gmail.com>
2020-10-26 12:03:44 -0700

    adds error templates, content management, images

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,30 @@
 from flask import Flask, render_template
+from content_management import Content
 
 
 app = Flask(__name__)
+TOPIC_DICT = Content()
 
 
 @app.route('/')
 def homepage():
-    return render_template("main.html")
+    return render_template('main.html')
 
 @app.route('/dashboard/')
 def dashboard():
-    return render_template("dashboard.html")
+    return render_template('dashboard.html', TOPIC_DICT=TOPIC_DICT)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
+
+
+@app.route('/fake/')
+def fakeit():
+    try:
+        return render_template('dashboard.html', TOPIC_DICT=gibberish)
+    except Exception as e:
+        return render_template('500.html', error=e)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ app/content_management.py
@@ -0,0 +1,11 @@
+def Content():
+    TOPIC_DICT = {
+        'Basics': [
+            ['Intro', '/intro/'],
+            ['History', '/history/'],
+            ['Other', '/other/']
+        ],
+        'Web Dev': []
+    }
+
+    return TOPIC_DICT
